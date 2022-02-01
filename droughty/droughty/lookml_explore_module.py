cf781ac0da19ba9c47cc1def629df6643171360c
Lewis <lewischarlesbaker@gmail.com>
2022-02-01 14:12:22 +0000

    redundant models removed

--- droughty/droughty/lookml_explore_module.py
+++ droughty/droughty/lookml_explore_module.py
@@ -15,6 +15,7 @@ import yaml
 import git
 
 from lookml_base_dict import d2
+from lookml_base_dict import distinct_duplicate_explore_rows
 
 
 def get_all_values(nested_dictionary):
@@ -33,21 +34,60 @@ def get_all_values(nested_dictionary):
         
         yield(looker.dump(explore))
 
-        for key1 in value.keys():     
+        for key1 in value.keys():  
 
-            join = {
+            if key1[0] == key1[0] and key1[0] not in distinct_duplicate_explore_rows:
+
+                join = {
+
+                    "joins": [
+                        {
+                        "sql_on": key1[0]+"."+key1[3]+" = "+ key1[2]+"."+key1[1],
+                        "relationship": key1[4],
+                        "name": key1[0]
+                        }
+
+                    ]
+                    }
+
+                yield(looker.dump(join))
+
+
+            elif key1[2] == key1[2] and key1[0] not in distinct_duplicate_explore_rows:
+
+                join = {
 
                 "joins": [
                     {
-                    "sql_on": key1[0]+"."+key1[1]+" = "+ key1[2]+"."+key1[3],
+                    "sql_on": key1[2]+"."+key1[3]+" = "+ key1[0]+"."+key1[1],
                     "relationship": key1[4],
-                    "name": key1[0]
+                    "name": key1[2]
                     }
 
                 ]
                 }
 
-            yield(looker.dump(join))
+                yield(looker.dump(join))
+
+
+            if key1[0] in distinct_duplicate_explore_rows:
+
+
+                join = {
+
+                    "joins": [
+                        {
+                        "sql_on": key1[0]+"."+key1[3]+" = "+ key1[2]+"."+key1[1],
+                        "relationship": key1[4],
+                        "from": key1[0],
+                        "name": key1[2]+"_"+key1[0]
+                        }
+
+                    ]
+                    }
+
+                yield(looker.dump(join))
+
 
 
 nested_dictionary = d2

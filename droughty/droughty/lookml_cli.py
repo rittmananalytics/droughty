6e50a37bd3f93f416724d72e6923aa9c0b30c8dd
Lewis <lewischarlesbaker@gmail.com>
2022-02-04 15:08:29 +0000

    progress bars and colours added

--- droughty/droughty/lookml_cli.py
+++ droughty/droughty/lookml_cli.py
@@ -1,5 +1,7 @@
 """Console script for droughty."""
 import typer
+import time
+from tqdm import tqdm
 
 from lookml_module import output
 from lookml_explore_module import explore_output
@@ -9,22 +11,61 @@ app = typer.Typer()
 
 
 @app.command()
+
 def base():
 
     typer.echo(f"Generating lookml base layer")
 
-    return output()
+    total = 0
+    with typer.progressbar(range(100), label = "Processing") as progress:
+        for value in progress:
+            # Fake processing time
+            time.sleep(0.01)
+            total += 1
+    try:
+
+        return output()
+
+    finally:
+
+        print ("lookml base layer generated")
 
 @app.command()
+
 def explore():
 
     typer.echo(f"Generating explore layer")
 
-    return explore_output()
+    total = 0
+    with typer.progressbar(range(100), label = "Processing") as progress:
+        for value in progress:
+            # Fake processing time
+            time.sleep(0.01)
+            total += 1
+    try:
+
+        return explore_output()
+
+    finally:
+
+        print ("lookml explore layer generated")
 
 @app.command()
+
 def measures():
 
     typer.echo(f"Generating measure layer")
 
-    return measure_output()
+    total = 0
+    with typer.progressbar(range(100), label = "Processing") as progress:
+        for value in progress:
+            # Fake processing time
+            time.sleep(0.01)
+            total += 1
+    try:
+
+        return measure_output()
+
+    finally:
+
+        typer.secho("lookml measure layer generated",fg=typer.colors.GREEN)

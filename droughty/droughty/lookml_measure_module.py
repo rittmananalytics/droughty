bfdeb4348c8a5b381f9c4d8b1feedf43a372ac9c
Lewis <lewischarlesbaker@gmail.com>
2022-03-24 21:09:02 +0000

    includes path corrected

--- droughty/droughty/lookml_measure_module.py
+++ droughty/droughty/lookml_measure_module.py
@@ -22,7 +22,7 @@ def get_all_values(nested_dictionary):
 
     includes = {
 
-        "includes": ["/_base/base.layer.lkml" ]
+        "includes": ["/base/_base.layer.lkml" ]
             
     }
 

4945195b8e7b78e1bae9d1d576abd666e84c77bc
Lewis <lewischarlesbaker@gmail.com>
2022-04-08 14:11:35 +0100

    modelled explore, base and measure cube layers

---
+++ droughty/droughty/cube_measure_module.py
@@ -0,0 +1,115 @@
+import lkml as looker
+from pprint import pprint
+from google.oauth2 import service_account
+import pandas_gbq
+from contextlib import redirect_stdout
+import snowflake.connector
+from sqlalchemy import create_engine
+from snowflake.sqlalchemy import URL
+import pandas as pd
+import pandas
+import os
+import json
+import sys
+import yaml
+import git
+
+import cube_parser.cube as cube
+
+from droughty.cube_base_dict import d1
+from droughty.config import schema_name
+
+def get_all_values(nested_dictionary):
+
+    for key,value in nested_dictionary.items():
+
+        explore = {
+
+
+            "cube": key+"_extended",
+            "sql": "select * from"+" "+schema_name+"."+key,
+            "extends": key+",",
+            "measures": '{'
+ 
+        }
+            
+        
+        yield(cube.dump(explore))
+                            
+
+        for key, value in value.items():
+            
+            if "pk" in key[0]:
+
+
+                measure = {
+
+
+                    "measure": {
+                    "sql": key[0],
+                    "type": "count",
+                    "name": "count_of_"+key[0]
+
+                    }
+
+                }
+
+                yield(cube.dump(measure))
+                
+
+            if key[1] == 'number':
+
+                measure = {
+
+
+                    "measure": {
+                    "sql": key[0],
+                    "type": "sum",
+                    "name": "sum_of_"+key[0]
+
+                    }
+
+                }
+
+                yield(cube.dump(measure))
+
+                
+        for key,value in nested_dictionary.items():
+            
+            closing_syntax = "}});"
+            
+        yield (closing_syntax)
+                
+
+nested_dictionary = d1
+
+get_all_values(nested_dictionary)
+
+def get_git_root(path):
+
+        git_repo = git.Repo(path, search_parent_directories=True)
+        git_root = git_repo.git.rev_parse("--show-toplevel")
+        return (git_root)
+
+git_def_path = get_git_root(os.getcwd())
+
+def output():
+    
+    git_path = git_def_path
+
+    rel_path = "schema"
+
+    path = os.path.join(git_path, rel_path)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+        
+    filename = 'cube.js'
+
+    with open(os.path.join(path, filename), 'w') as file:
+
+        with redirect_stdout(file):
+
+                for value in get_all_values(nested_dictionary):
+
+                    print(value)

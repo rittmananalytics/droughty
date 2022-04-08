67a5efac8bbb923aafc183ce380c4b8a74a4413f
Lewis <lewischarlesbaker@gmail.com>
2022-04-08 14:20:01 +0100

    cli init

--- droughty/droughty/main.py
+++ droughty/droughty/main.py
@@ -3,12 +3,14 @@ import typer
 from droughty import lookml_cli 
 from droughty import dbt_test_cli
 from droughty import dbml_cli
+from droughty import cube_cli
 
 app = typer.Typer()
 
 app.add_typer(lookml_cli.app, name="lookml")
 app.add_typer(dbt_test_cli.app, name="dbt")
 app.add_typer(dbml_cli.app, name="dbml")
+app.add_typer(cube_cli.app, name="cube")
 
 if __name__ == "__main__":
     app()

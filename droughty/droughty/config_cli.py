aede8412d6a3fd094f5f63c717d24963daf0d5bc
Lewis <lewischarlesbaker@gmail.com>
2022-05-06 12:13:08 +0100

    working cli class

--- droughty/droughty/config_cli.py
+++ droughty/droughty/config_cli.py
@@ -9,10 +9,29 @@ class Common:
 
     profile_dir: str
 
-Common.profile_dir = "/Users/lewischarlesbaker/.droughty/profile.yaml"
+#Common.profile_dir = "/Users/lewischarlesbaker/.droughty/profile.yaml"
 
-profile_parser = argparse.ArgumentParser(description='Say hi.')
-profile_parser.add_argument('--profile-dir', type=str, help='the directory of the profile')
+def some_function_1(profile_dir):
+    """Some example funcion"""
+    msg = profile_dir
+    print(msg + "function test")
+    #Common.profile_dir = profile_dir
+
+def profile_func():
+
+    profile_parser = argparse.ArgumentParser(description='Say hi.')
+    profile_parser.add_argument('--profile-dir', type=str, help='the directory of the profile')
+
+    args = profile_parser.parse_args()
+
+    some_function_1(args.profile_dir)
+
+    Common.profile_dir = (args.profile_dir)
+
+    #Common.profile_dir = "/Users/lewischarlesbaker/.droughty/profile.yaml"
+ 
+profile_func()
+
 
 #def argument_profile(profile_dir):
 #
--- droughty/droughty/main.py
+++ droughty/droughty/main.py
@@ -2,7 +2,7 @@ from droughty.lookml_cli import base
 #from droughty import dbt_test_cli
 #from droughty import dbml_cli
 #from droughty import cube_cli
-from droughty.config_cli import profile_parser#,argument_profile
+from droughty.config_cli import profile_func #profile_parser#,argument_profile
 
 ##app.add_typer(dbt_test_cli.app, name="dbt")
 ##app.add_typer(dbml_cli.app, name="dbml")
@@ -14,14 +14,14 @@ from droughty.config_cli import profile_parser#,argument_profile
 
 import argparse
 
-class ArgParseProfileDir(object):
-
-  def __init__(self):
-    self.path = args.profile_dir
-
-
-  def retain_me(self):
-    print (self.path)
+#class ArgParseProfileDir(object):
+#
+#  def __init__(self):
+#    self.path = args.profile_dir
+#
+#
+#  def retain_me(self):
+#    print (self.path)
 
 def some_function(profile_dir):
     """Some example funcion"""
@@ -35,36 +35,17 @@ def start():
     # All the logic of argparse goes in this function
 
     parser = argparse.ArgumentParser(description='Say hi.')
-    #parser.add_argument('--profile-dir', type=str, help='the directory of the profile')
-    #parser.add_argument('--profile-dir-test', type=str, help='the directory of the profile')
 
     parser.add_argument('--end', dest='end', default="!",
                     help='sum the integers (default: find the max)')
-
-    args = profile_parser.parse_args()
-
-    #some_function(args.profile_dir)
-
-    #c1 = ArgParseProfileDir(args.profile_dir)
-    args_dict = vars(args)
-    #c2 = ArgParseProfileDir(**args_dict)
-
-    #print(c1)
-
+    profile_func()
     base()
-
-    shell = ArgParseProfileDir()
-    shell.retain_me()
-
-    foo = (args.profile_dir)
 
     sub_parsers = parser.add_subparsers(help='sub-command help')
 
-    # create the parser for the "ahoy" sub-command
     parser_ahoy = sub_parsers.add_parser('lookml', help='lookml is cool sub-command')
     parser_ahoy.add_argument('--bar', type=int, help='bar is useful option')
 
-print (ArgParseProfileDir.retain_me)
 
 
 if __name__ == '__main__':
